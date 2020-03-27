111c88a6c4cf591d5b83b4e62c15389d15e1d46e
nicolas holland <dutchman@linux.fritz.box>
2020-03-27 15:52:52 +0100

    removed trailing whitespaces

--- fileDownload.py
+++ fileDownload.py
@@ -3,8 +3,8 @@
 
 import os
 import os.path
-import requests 
-from bs4 import BeautifulSoup 
+import requests
+from bs4 import BeautifulSoup
 import pygrib
 import tarfile
 from pvlib.forecast import GFS
@@ -18,45 +18,45 @@ from pylab import rcParams
 
 
 
-def get_file_links(url): 
-      
-    # create response object 
-    r = requests.get(url) 
-    # create beautiful-soup object 
-    soup = BeautifulSoup(r.content,'html')  
-    # find all links on web-page 
-    links = soup.findAll('a') 
+def get_file_links(url):
+
+    # create response object
+    r = requests.get(url)
+    # create beautiful-soup object
+    soup = BeautifulSoup(r.content,'html')
+    # find all links on web-page
+    links = soup.findAll('a')
     # filter the link sending with .grb2
-    tar_links = [url + link['href'] for link in links if link['href'].endswith('.tar')] 
+    tar_links = [url + link['href'] for link in links if link['href'].endswith('.tar')]
     return tar_links
 
-def download_tar_files(file_link, dest_path): 
-    
+def download_tar_files(file_link, dest_path):
+
     try:
         os.mkdir(dest_path)
     except OSError:
         print ("Path %s already exists" % dest_path)
     else:
         print ("Successfully created the directory %s " % dest_path)
-  
-    for link in file_link: 
-  
-        '''iterate through all links in video_links 
-        and download them one by one'''  
-        # obtain filename by splitting url and getting  
-        # last string     
-        file_name = link.split('/')[-1]    
+
+    for link in file_link:
+
+        '''iterate through all links in video_links
+        and download them one by one'''
+        # obtain filename by splitting url and getting
+        # last string
+        file_name = link.split('/')[-1]
         target_path = os.path.join(dest_path, file_name)
-          
-        # create response object 
-        r = requests.get(link, stream = True) 
-          
-        # download started 
-        with open(target_path, 'wb') as f: 
-            for chunk in r.iter_content(chunk_size = 1024*1024): 
-                if chunk: 
-                    f.write(chunk)  
-  
+
+        # create response object
+        r = requests.get(link, stream = True)
+
+        # download started
+        with open(target_path, 'wb') as f:
+            for chunk in r.iter_content(chunk_size = 1024*1024):
+                if chunk:
+                    f.write(chunk)
+
     print("All files are downloaded!")
     return
 
@@ -70,12 +70,12 @@ def extract_grib_files(dir_name, file_ext):
             tar.extractall(dir_name)
             tar.close()
             os.remove(os.path.join(dir_name,file_name))
-            
-            
+
+
 def grib_to_df(filename, location, var, var2):
     '''
     Converts grib data to dataframe for given location
-    
+
     Parameters
     -------------------------------------------------
     filename: complete path of grib file
@@ -85,28 +85,28 @@ def grib_to_df(filename, location, var, var2):
     lat2 = location[2]
     lon1 = location[1]
     lon2 = location[3]
-    
+
     grbs = pygrib.open(filename)
-    
+
     try:
         with pygrib.open(filename) as grbs:
             grb = grbs.select(name=var)[0]
             data, lats, lons = grb.data(lat1=lat1, lat2=lat2, lon1=lon1, lon2=lon2)
             data_flat = np.reshape(data, (np.product(data.shape),))
-            
+
             grb_2 = grbs.select(name=var2)[0]
             data2 = grb_2.data(lat1=lat1, lat2=lat2, lon1=lon1, lon2=lon2)[0]
             data2_flat = np.reshape(data2, (np.product(data2.shape),))
-            
+
             lats_flat = np.reshape(lats, (np.product(lats.shape),))
             lons_flat = np.reshape(lons, (np.product(lons.shape),))
-            
+
             #datetime1 = pd.to_datetime("%d0%d0%d 0"  str(data.year)+str('0')+str(data.month)+str('0') + str(data.day)+str(' 0')+str(data.hour))
-            
+
             name2step = lambda name :pd.Timedelta(hours=int(name[-8:-5]))
             base = datetime.datetime(year=grb.year, month=grb.month, day=grb.day, hour=grb.hour)
             valid = base + name2step(filename)
-            
+
             df = pd.DataFrame({'validtime': valid,
                                'basetime': base,
                                'latitude': lats_flat,
@@ -117,8 +117,8 @@ def grib_to_df(filename, location, var, var2):
             return df
 
     except ValueError:
-        	print("Parameter not found: {}".format(filename))  
-        
+        	print("Parameter not found: {}".format(filename))
+
 def convert_tar_to_csv(sourcepath, destinationpath, filename, location):
     files = glob.glob(os.path.join(sourcepath, "*grb2"))
     files.sort()
@@ -130,13 +130,13 @@ def convert_tar_to_csv(sourcepath, destinationpath, filename, location):
         os.remove(file)
 
 
-if __name__ == '__main__':    
+if __name__ == '__main__':
     url = "https://www1.ncdc.noaa.gov/pub/has/model/HAS011478636/"
     grb_filepath = '/home/saptaparni/gfsdata/'
     tar_links = get_file_links(url)
     download_tar_files(tar_links, grb_filepath)
     extract_grib_files(grb_filepath, '.tar')
-    
+
     # bounding box of the location for which you want to get GHI values
     #location = [48,8,48.1,8]
     # csv file name format: <YYYY_STARTMONTH_ENDMONTH.csv>
@@ -144,4 +144,4 @@ if __name__ == '__main__':
     # file path to save the csv files
     #csv_filepath = '/scratch/data/gfscsvfiles/'
     #convert_tar_to_csv(grb_filepath, csv_filepath, filename)
-    
+
